d5135224210700c1e5bc8c14568973f3a0bd49ed
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-07-19 21:41:26 -0400

    fix so approve application :status gets updated

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,7 +2,19 @@ class Admin::ApplicationsController < ApplicationController
     def show
       @application = Application.find(params[:id])
       @pets = @application.pets
+      @application.pets.each do |pet| 
+      application_pet = @application.pet_app_find(pet.id)
+      if application_pet.status == "Approved"
+        @application.update(status: "Approved")
+        redirect_to '/admin/shelters'
+      end
+      if application_pet.status == "Rejected"
+        @application.update(status: "Rejected")
+      end
     end
+    end
+   
+ 
   
   
   end
