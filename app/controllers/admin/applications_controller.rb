50fb409a2070d548f372e4c4f6ca022d3312abe5
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-07-19 23:10:21 -0400

    edit testing

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -6,7 +6,6 @@ class Admin::ApplicationsController < ApplicationController
       application_pet = @application.pet_app_find(pet.id)
       if application_pet.status == "Approved"
         @application.update(status: "Approved")
-        redirect_to '/admin/shelters'
       end
       if application_pet.status == "Rejected"
         @application.update(status: "Rejected")
